40058f907c6537d06c6bb03e8d242a7c0503a86b
Mohamed-mo12 <114833143+Mohamed-mo12@users.noreply.github.com>
2024-02-05 20:30:53 +0200

    Tasks solution

---
+++ Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 1 
+            /*
+            Console.WriteLine(" Enter your name ");
+
+            string user_name = Console.ReadLine();
+
+            Console.WriteLine($" Hello mr {user_name} ");
+
+
+            Console.WriteLine("-------------------------------------------------");
+            
+
+            // 2 
+
+            Console.WriteLine(" Enter First number ");
+
+            float first_number = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(" Enter second number ");
+
+            float second_number = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($" addition : {first_number + second_number}");
+            Console.WriteLine($" subtraction : {first_number - second_number}");
+            Console.WriteLine($" multiplication : {first_number * second_number}");
+            Console.WriteLine($" division : {first_number / second_number}");
+
+            
+
+            // 3 
+
+            Console.WriteLine("Enter first number");
+            int first_number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter secnod number");
+            int second_number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter third number");
+            int third_number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter fourth number");
+            int fourth_number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($" Average : {(first_number+second_number+third_number+fourth_number) / 4  } ");
+
+            
+
+            // 4 
+
+            Console.WriteLine("Enter first number");
+
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Third number");
+            int z = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($" Results is : {(x+y)*z} and {x*y + y*z} ");
+            
+
+            // 5 
+
+
+            Console.WriteLine("Enter your age");
+
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"you look older than {age} ");
+            
+
+             // 6 
+            Console.WriteLine("Enter amount of celsius");
+            int celsius = int.Parse(Console.ReadLine());
+            // kelvin 
+            Console.WriteLine($"Kelvin degree is : {celsius + 273}");
+            // fahrenhiet
+            Console.WriteLine($"fahrenhirt degree is : {celsius*18/10+32}");
+
+            // 7
+
+            Console.WriteLine(" Enter your words ");
+
+            string name = "MOHAMED HASSAN ";
+
+            Console.WriteLine($" new shape of words is {name.ToLower()}");
+
+       
+            // 8
+            Console.WriteLine(" enter string");
+
+            string name = Console.ReadLine(); //  assign name wael 
+
+            bool result = name.Contains('w');
+
+            Console.WriteLine($" {result}");
+
+            
+
+
+            Console.WriteLine("Enter the string ");
+
+            string words = Console.ReadLine();
+          
+
+            Console.WriteLine($" total number of words is : {words.Count()}");
+
+            */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey(); 
+
+            
+
+
+
+
+        }
+    }
+}
